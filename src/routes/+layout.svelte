bcf55ea5660ab8a439e4c44883fc20b2e8ea7a13
Paulio <PaulioRandall@gmail.com>
2024-04-07 11:35:54 +0100

    Add sveltekit to use as test pages

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<div class="page">
+	<div class="content">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.page {
+		display: flex;
+		flex-direction: column;
+
+		align-items: center;
+		justify-content: center;
+
+		min-height: 100svh;
+	}
+
+	.content {
+		flex-grow: 1;
+
+		max-width: 100%;
+		width: 100%;
+	}
+</style>
